66fa2eb4f2e35b2212e4e5931bf5725e503140b7
Qara Yahya <qara.yahya@gmail.com>
2025-05-07 16:34:10 +0100

    Refactor block styles registration and improve code organization

--- inc/block-styles.php
+++ inc/block-styles.php
@@ -4,56 +4,85 @@
  *
  * @package flaron
  * @author Yahya Qara
- * @since 0.0.4
+ * @since 0.0.7
  */
-
-namespace flaron;
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-/**
- * Register block styles
- */
-$dark_style = array(
-	'background-color' => 'var(--wp--preset--color--dark-gray)',
-	'color'            => 'var(--wp--preset--color--light-gray)',
-);
+// Registers custom block styles.
+if ( ! function_exists( 'flaron_block_styles' ) ) :
 
-// Register block styles for core/code and core/preformatted.
-register_block_style(
-	'core/code',
-	array(
-		'name'         => 'dark-code',
-		'label'        => __( 'Dark', 'flaron' ),
-		'inline_style' => flaron_generate_inline_style( $dark_style ),
-	)
-);
+	function flaron_block_styles() {
 
-register_block_style(
-	'core/preformatted',
-	array(
-		'name'         => 'dark-preformatted',
-		'label'        => __( 'Dark', 'flaron' ),
-		'inline_style' => flaron_generate_inline_style( $dark_style ),
-	)
-);
+		/**
+		 * Register block styles
+		 */
+		$dark_style = array(
+			'background-color' => 'var(--wp--preset--color--dark-gray)',
+			'color'            => 'var(--wp--preset--color--light-gray)',
+		);
 
-// Register block styles for core/button.
-register_block_style(
-	'core/button',
-	array(
-		'name'         => 'secondary-button',
-		'label'        => __( 'Secondary', 'flaron' ),
-		'inline_style' => '
-            .is-style-secondary-button .wp-element-button {
-                color: var(--wp--preset--color--primary);
-                background-color: var(--wp--preset--color--light-gray);
+		// Register block styles for core/code and core/preformatted.
+		register_block_style(
+			'core/code',
+			array(
+				'name'         => 'dark-code',
+				'label'        => __( 'Dark', 'flaron' ),
+				'inline_style' => flaron_generate_inline_style( $dark_style ),
+			)
+		);
+
+		register_block_style(
+			'core/preformatted',
+			array(
+				'name'         => 'dark-preformatted',
+				'label'        => __( 'Dark', 'flaron' ),
+				'inline_style' => flaron_generate_inline_style( $dark_style ),
+			)
+		);
+
+		// Register block styles for core/button.
+		register_block_style(
+			'core/button',
+			array(
+				'name'         => 'secondary-button',
+				'label'        => __( 'Secondary', 'flaron' ),
+				'inline_style' => '
+					.is-style-secondary-button .wp-element-button {
+						color: var(--wp--preset--color--primary);
+						background-color: var(--wp--preset--color--light-gray);
+					}
+        		',
+			)
+		);
+
+		// Register block styles for core/tag-cloud.
+		register_block_style(
+			'core/tag-cloud',
+			array(
+				'name'         => 'button',
+				'label'        => __( 'Button', 'flaron' ),
+				'inline_style' => '
+            .wp-block-tag-cloud.is-style-button {
+                display: flex;
+                flex-wrap: wrap;
+                gap: 0.65rem;
+            }
+            .wp-block-tag-cloud.is-style-button a {
+                border-radius: var(--wp--custom--border-radius--full);
+                padding: 0.65rem 1rem;
+                text-decoration: none !important;
+                background-color: #f1f5f9;
             }
         ',
-	)
-);
+			)
+		);
+	}
+endif;
+add_action( 'init', 'flaron_block_styles' );
+
 
 /**
  * Generate inline style from an array of CSS properties.
@@ -68,26 +97,3 @@ function flaron_generate_inline_style( $styles ) {
 	}
 	return $inline_style;
 }
-
-
-// Register block styles for core/tag-cloud.
-register_block_style(
-	'core/tag-cloud',
-	array(
-		'name'         => 'button',
-		'label'        => __( 'Button', 'flaron' ),
-		'inline_style' => '
-            .wp-block-tag-cloud.is-style-button {
-                display: flex;
-                flex-wrap: wrap;
-                gap: 0.65rem;
-            }
-            .wp-block-tag-cloud.is-style-button a {
-                border-radius: var(--wp--custom--border-radius--full);
-                padding: 0.65rem 1rem;
-                text-decoration: none !important;
-                background-color: #f1f5f9;
-            }
-        ',
-	)
-);
